024ac6c2476e3b97659a27e06cfc53edbbe3ed91
BorjaZarco <borjia98@gmail.com>
2018-08-16 00:49:02 +0100

    Hello World

--- index.js
+++ index.js
@@ -2,15 +2,9 @@ const usersRouter = require('./api/users');
 const express = require('express');
 const app = express();
 
-
-const config = require('./.env');
-const options = config[process.env.NODE_ENV];
-const _PORT = options.PORT;
-
-
 //middlewares
 app.use(express.json());
 
 app.use('/api/users', usersRouter);
 
-app.listen(_PORT);
+app.listen(5000);
